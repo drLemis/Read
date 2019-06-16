fa46ec5d402c0e40001b731b20a4cd1b9126a679
Lemis <remminarsettleher@mail.ru>
2019-06-16 14:10:52 +0300

    Style load rework

--- js/loadText.js
+++ js/loadText.js
@@ -35,6 +35,7 @@ function changePart(partDeta) {
 }
 
 function getPartList() {
+    console.log(1)
     fetch("https://raw.githubusercontent.com/drLemis/Read/master/text/partList.txt")
         .then(function (response) {
             response.text().then(function (text) {
@@ -49,11 +50,11 @@ function getPartList() {
 }
 
 function changeStyle(name) {
-    var oldlink = document.getElementsByTagName("link").item(0);
+    var oldlink = document.getElementsByTagName("link").item(2);
     var newlink = document.createElement("link");
     newlink.setAttribute("rel", "stylesheet");
     newlink.setAttribute("type", "text/css");
     newlink.setAttribute("href", "css/styleTextColors" + name + ".css");
-
+    
     document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
 }
