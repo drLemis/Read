342f8890c4e37e77b161b921352d6b0e6f968257
Lemis <remminarsettleher@mail.ru>
2019-06-16 13:45:08 +0300

    Chapters load rework

--- js/loadText.js
+++ js/loadText.js
@@ -1,44 +1,59 @@
 var partIndex = 0;
+var partStyle = "";
 
 var partList = [];
 
-function changePart(partDeta)
-{
+function changePart(partDeta) {
     partIndex += partDeta;
-
-    console.log(partList)
 
     if (partIndex < 0)
         partIndex = 0;
     else if (partIndex > [partList.length - 1])
         partIndex = partList.length - 1;
 
-    var partIndexVar = partIndex;
+    var partIndexNew = partIndex;
+    var partStyleNew = partStyle;
 
+    //Точная ссылка на главу
     if (partList[partIndex] && partList[partIndex].length > 1)
-        partIndexVar = partList[partIndex][1];
+        partIndexNew = partList[partIndex][1];
 
-    fetch("https://raw.githubusercontent.com/drLemis/Read/master/text/" + partIndexVar)
+    //Особый стиль главы
+    if (partList[partIndex] && partList[partIndex].length > 2)
+        partStyleNew = partList[partIndex][2];
+
+    fetch("https://raw.githubusercontent.com/drLemis/Read/master/text/" + partIndexNew + ".html")
         .then(function (response) {
             response.text().then(function (text) {
                 document.getElementsByClassName("textBodyText")[0].innerHTML = text;
                 document.getElementsByClassName("textBodyHeaderName")[0].innerHTML = partList[partIndex][0];
 
+                if (partStyleNew != "" && partStyleNew != partStyle)
+                    changeStyle(partStyleNew);
             });
         });
 }
 
-function getPartList()
-{
-    fetch("https://raw.githubusercontent.com/drLemis/Read/master/text/partList")
-    .then(function (response) {
-        response.text().then(function (text) {
-            var parts = text.split("\n");
-            parts.forEach(part => {
-                partList.push(part.split("@"));
-            }); 
+function getPartList() {
+    fetch("https://raw.githubusercontent.com/drLemis/Read/master/text/partList.txt")
+        .then(function (response) {
+            response.text().then(function (text) {
+                var parts = text.split("\n");
+                parts.forEach(part => {
+                    partList.push(part.split("@"));
+                });
 
-            changePart(0);
+                changePart(0);
+            });
         });
-    });
+}
+
+function changeStyle(name) {
+    var oldlink = document.getElementsByTagName("link").item(0);
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", "css/styleTextColors" + name + ".css");
+
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
 }
